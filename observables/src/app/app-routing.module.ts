c31725c2af2a9d9b76f02d29bb4f0d9a3b0a144c
Foodie <olaorefouad565@gmail.com>
2019-03-28 10:37:40 +0100

    Completed the routing section. I now know how to properly integrate routes, pass query params, implement auth guards, and pass in static data to routes.

---
+++ observables/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'user/:id', component: UserComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
